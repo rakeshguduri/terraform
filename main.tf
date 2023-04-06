5af6c084062a4095a883dcdbbb42f8c78a241999
rakeshguduri <124152798+rakeshguduri@users.noreply.github.com>
2023-04-06 16:55:45 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">4.32.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "vm" {
+  ami                         = "ami-007855ac798b5175e"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  security_groups             = ["free"]
+  key_name                    = "kasi"
+
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> private_ips.txt"
+
+  }
+  tags = {
+    Name = "ec2"
+  }
+
+}
